9ea65b53ee73a0b657508cd3b1b77fb84b4b72fa
Aditya <aditya.r.mhatre@gmail.com>
2018-03-09 22:42:56 -0600

    Problem 1 solution

---
+++ Problem 2/Problem 2.py
@@ -0,0 +1,11 @@
+from functools import reduce
+fibs = list()
+fibs.append(1)
+fibs.append(2)
+i = 2
+while fibs[i - 1] + fibs[i - 2] < 4000000:
+    fibs.append(fibs[i - 1] + fibs[i - 2])
+    i += 1
+
+
+print(reduce(lambda a, b: a + b, [n for n in fibs if n % 2 == 0]))
