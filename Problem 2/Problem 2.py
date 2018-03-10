260f23d8e1d6697ab5f358dae77f552ec9bc4b35
Aditya <aditya.r.mhatre@gmail.com>
2018-03-09 22:52:31 -0600

    Updated using Functional Programming for Problem 2 solution

--- Problem 2/Problem 2.py
+++ Problem 2/Problem 2.py
@@ -6,4 +6,13 @@ while fibs[i - 1] + fibs[i - 2] < 4000000:
     fibs.append(fibs[i - 1] + fibs[i - 2])
     i += 1
 
-print(reduce(lambda a, b: a + b, [n for n in fibs if n % 2 == 0]))
+
+def is_even(n):
+    return n % 2
+
+
+def add(a, b):
+    return a + b
+
+
+print(reduce(add, filter(is_even, fibs)))
