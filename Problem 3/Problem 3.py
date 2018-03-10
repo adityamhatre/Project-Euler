912fa1484bb54dc8bff42914b2197e927363d2c7
Aditya <aditya.r.mhatre@gmail.com>
2018-03-09 23:49:04 -0600

    Problem 3 solution

--- Problem 3/Problem 3.py
+++ Problem 3/Problem 3.py
@@ -1,1 +1,16 @@
-def get_prime_factors(x):
+def max_prime_factor(x):
+    a = x
+    b = 2
+    c = None
+    while b < a / 2:
+        if a % b == 0:
+            if c is not None and b > c:
+                c = b
+            a = a / b
+            b = 2
+        else:
+            b += 1
+    return a
+
+
+print(max_prime_factor(600851475143))
